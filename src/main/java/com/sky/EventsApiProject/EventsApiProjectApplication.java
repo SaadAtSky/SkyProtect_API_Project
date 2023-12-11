f28872ffcea5c474ba6bba8e1baa972b0837dc20
Saad Ahmad <saad.ahmad@sky.uk>
2023-12-11 20:20:11 +0000

    initial commit

---
+++ src/main/java/com/sky/EventsApiProject/EventsApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sky.EventsApiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/EventsApiProject/EventsApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.EventsApiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
