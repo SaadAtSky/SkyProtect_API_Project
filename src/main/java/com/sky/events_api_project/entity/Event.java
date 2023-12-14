9a05ab04631d5e6486caeaac9adc6151cc4be638
Saad Ahmad <saad.ahmad@sky.uk>
2023-12-14 09:01:57 +0000

    feat: added the 'category' column to the event table

--- src/main/java/com/sky/events_api_project/entity/Event.java
+++ src/main/java/com/sky/events_api_project/entity/Event.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -11,24 +12,35 @@ import lombok.Data;
 @Entity
 public class Event {
     @Id
+    @Column
     @Pattern(regexp = "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$",message = "Not a valid UUID")
     private String uuid;
 
+    @Column
     @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$", message = "not a valid date and time format")
     private String recorded_at;
 
+    @Column
     @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$", message = "not a valid date and time format")
     private String received_at;
 
+    @Column
+    @NotBlank
+    private String category;
+
+    @Column
     @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$", message = "not a valid date and time format")
     private String created_at;
 
+    @Column
     @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$", message = "not a valid date and time format")
     private String updated_at;
 
+    @Column
     @Pattern(regexp = "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$",message = "Not a valid UUID")
     private String device_uuid;
 
+    @Column
     private JsonNode metadata;
 
     @Column(columnDefinition = "boolean default false")
