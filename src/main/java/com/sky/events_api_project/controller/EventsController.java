ca775ac99e440b4cdc9b20fb21606e5b6f4bf9d2
Saad Ahmad <saad.ahmad@sky.uk>
2023-12-15 11:42:11 +0000

    feat: added category column in event, added prefix to api endpoint urls

--- src/main/java/com/sky/events_api_project/controller/EventsController.java
+++ src/main/java/com/sky/events_api_project/controller/EventsController.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/event")
 public class EventsController {
     private final EventsService eventsService;
 
--- src/main/java/com/sky/events_api_project/entity/Event.java
+++ src/main/java/com/sky/events_api_project/entity/Event.java
@@ -1,9 +1,9 @@
 package com.sky.events_api_project.entity;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -11,24 +11,35 @@ import lombok.Data;
 @Entity
 public class Event {
     @Id
+    @Column
     @Pattern(regexp = "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$",message = "Not a valid UUID")
     private String uuid;
 
+    @Column
     @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$", message = "not a valid date and time format")
     private String recorded_at;
 
+    @Column
     @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$", message = "not a valid date and time format")
     private String received_at;
 
+    @Column
+    @NotBlank
+    private String category;
+
+    @Column
     @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$", message = "not a valid date and time format")
     private String created_at;
 
+    @Column
     @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$", message = "not a valid date and time format")
     private String updated_at;
 
+    @Column
     @Pattern(regexp = "^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$",message = "Not a valid UUID")
     private String device_uuid;
 
+    @Column
     private JsonNode metadata;
 
     @Column(columnDefinition = "boolean default false")
@@ -36,5 +47,4 @@ public class Event {
 
     @Column(columnDefinition = "boolean default false")
     private Boolean is_deleted;
-
 }
