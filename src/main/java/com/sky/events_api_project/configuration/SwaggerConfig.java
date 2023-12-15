4302119d1ab3890e293637e5fd688f40aee8b0b3
Saad Ahmad <saad.ahmad@sky.uk>
2023-12-15 11:22:05 +0000

    feat: added swagger documentation, added prefix to api endpoints

---
+++ src/main/java/com/sky/events_api_project/configuration/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.sky.events_api_project.configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Sky Protect Events API project")
+                .description("simple Events API containing endpoints to create, update, view, and export event records")
+                .version("1.0.0")
+                .contact(new Contact("Saad Ahmad", "www.TBD.com", "saad.ahmad@sky.com"))
+                .build();
+    }
+}
